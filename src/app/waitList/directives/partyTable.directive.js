dcdb6e5e7c80ffdcfc7fb7cac31fa5a06570dd14
Hardik Sondagar <hardikmsondagar@gmail.com>
2015-12-24 18:20:50 +0530

    CORS request temp shifted to ajax

--- src/app/waitList/directives/partyTable.directive.js
+++ src/app/waitList/directives/partyTable.directive.js
@@ -1,67 +1,69 @@
 (function() {
-  'use strict';
+	'use strict';
 
-  angular
-  .module('app.waitList')
-  .directive('gzPartyTable', gzPartyTable);
+	angular
+	.module('app.waitList')
+	.directive('gzPartyTable', gzPartyTable);
 
-  function gzPartyTable() {
-    return {
-      templateUrl: 'app/waitList/directives/partyTable.html',
-      restrict: 'E',
-      controller: PartyTableController,
-      controllerAs: 'vm',
-      bindToController: true,
-      scope: {
-        parties: '=',
-        profile: '='
-      }
-    }
-  }
+	function gzPartyTable() {
+		return {
+			templateUrl: 'app/waitList/directives/partyTable.html',
+			restrict: 'E',
+			controller: PartyTableController,
+			controllerAs: 'vm',
+			bindToController: true,
+			scope: {
+				parties: '=',
+				profile: '='
+			}
+		}
+	}
 
-  PartyTableController.$inject = ['$http'];
+	PartyTableController.$inject = ['$http'];
 
-  function PartyTableController($http) {
-    var vm = this;
-    vm.removeParty = removeParty;
-    vm.sendTextMessage = sendTextMessage;
-    vm.toggleDone = toggleDone;
-    vm.loading=false;
+	function PartyTableController($http) {
+		var vm = this;
+		vm.removeParty = removeParty;
+		vm.sendTextMessage = sendTextMessage;
+		vm.toggleDone = toggleDone;
+		vm.loading=false;
 
-    function removeParty(party) {
-      party.deleted = Firebase.ServerValue.TIMESTAMP;
-      vm.parties.$save(party);
-    }
+		function removeParty(party) {
+			party.deleted = Firebase.ServerValue.TIMESTAMP;
+			vm.parties.$save(party);
+		}
 
 
-    function sendTextMessage(party) {
-      vm.loading=true;
-      var local = JSON.parse(localStorage.getItem('firebase:session::cozywait'));
-      var data = {
-       number: party.phone,
-       size: party.size,
-       name: party.name,
-       party_id : party.$id,
-       uid:local.uid,
-       token:local.token
-     };
-     
-     $http.post("https://www.mywedstory.com/cozywait/index.php", data).success(function(data, status) {
-      console.log("success");
-      vm.loading=false;
+		function sendTextMessage(party) {
+			vm.loading=true;
+			var local = JSON.parse(localStorage.getItem('firebase:session::cozywait'));
+			var data = {
+				number: party.phone,
+				size: party.size,
+				name: party.name,
+				party_id : party.$id,
+				uid:local.uid,
+				token:local.token
+			};
 
-      console.log(data);
-    }).error(function()
-    {
-      vm.loading=false;
-    });
+			$.ajax({
+				url : 'https://www.mywedstory.com/cozywait/index.php',
+				type : 'POST',
+				data : data,
+				dataType:'json',
+				success : function(data) {              
+					vm.loading=false;
+				},
+				error : function(request,error) {
+					vm.loading=false;
+				}
+			});
+		}
 
-  }
+		function toggleDone(party) {
+			vm.parties.$save(party);
+		}
 
-  function toggleDone(party) {
-    vm.parties.$save(party);
-  }
-
-}
+	}
 
 })();
